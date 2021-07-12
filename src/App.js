2bd85c8754b43c94c961cb09e2671fce1601fb24
Nikita Valkevich <n.valkevich@icloud.com>
2021-07-12 16:29:28 +0300

    init: add s2

--- src/App.js
+++ src/App.js
@@ -1,2 +1,25 @@
+import { useState } from 'react';
 import './App.css';
+import Button from './Button';
 
+const App = () => {
+  const [number, setNumber] = useState(0)
+
+  const plus = () => {
+    setNumber(number + 1)
+  }
+
+  const minus = () => {
+    setNumber(number - 1)
+  }
+
+  return (
+    <>
+      <Button label='-' onClick={minus} />
+        {number}
+      <Button label='+' onClick={plus} />
+    </>
+  )
+}
+
+export default App;
